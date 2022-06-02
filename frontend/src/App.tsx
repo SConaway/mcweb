8ee798825e7ef1893ce648530bc0eaae8ca9047f
Steven Conaway <steven.conaway@icloud.com>
2022-06-02 14:00:32 -0600

    add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Vite + React!</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/vite.config.ts
@@ -0,0 +1,19 @@
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+
+import {ViteTips} from 'vite-plugin-tips';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(), //
+        // ViteTips(),
+    ],
+    server: {
+        port: 4000,
+        proxy: {
+            '/list': 'http://localhost:3000',
+            '/send': 'http://localhost:3000',
+        },
+    },
+});
