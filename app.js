ff44f79a127ced9492890ffef0ecad65699c28cc
Steven Conaway <steven.conaway@icloud.com>
2022-06-01 20:11:05 -0600

    switch to modules

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-var app = express();
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+export default app;
