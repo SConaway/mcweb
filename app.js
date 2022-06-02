7cdc9c01608bfefcf3fb6cffdb721975ab9de5f5
Steven Conaway <steven.conaway@icloud.com>
2022-06-01 20:32:21 -0600

    modernize

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import helmet from 'helmet';
 
 import path from 'path';
 import {fileURLToPath} from 'url';
@@ -16,6 +17,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(logger('dev'));
+app.use(helmet());
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
